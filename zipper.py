3dfd63049bbe3e18c6cfd75a7a86d55ac3747776
carlwilson123 <caw169@pitt.edu>
2025-08-12 18:17:36 -0400

    Implement code changes to enhance functionality and improve performance need to fix folder issue

--- zipper.py
+++ zipper.py
@@ -1,5 +1,6 @@
-import zipfile #type:ignore
+# import shutil #type:ignore
 import os #type:ignore 
+import zipfile #type:ignore
 
 
 def zip_files(world_sel:list[str]|str = "all", user:str = os.getlogin(), rel_path:str = r"/AppData/Local/FoundryVTT/Data/worlds") -> None:
@@ -19,7 +20,7 @@ def zip_files(world_sel:list[str]|str = "all", user:str = os.getlogin(), rel_pat
             if world in world_sel:
                 worlds_to_zip.append(world)
                 #zip up all the files here
-    elif isinstance(world_sel, str):
+    elif isinstance(world_sel, str): #type:ignore
         if world_sel in worlds:
             sel_world = worlds[worlds.index(world_sel)]
             worlds_to_zip.append(sel_world)
@@ -27,10 +28,21 @@ def zip_files(world_sel:list[str]|str = "all", user:str = os.getlogin(), rel_pat
     else:
         print(f"{world_sel} is not a valid world name")
         
-    print(worlds_to_zip)
-        
-        
-        
+    paths:list[str] = [f"{path}{world}/" for world in worlds_to_zip]
+
+    #zipping files
+    
+    i = 0
+    for p in paths:
+        with zipfile.ZipFile(f"{worlds_to_zip[i]}.zip", 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, _, files in os.walk(p):
+                for file in files:
+                    
+                    file_path = os.path.join(root, file)
+                    print(file_path)
+                    # arcname = os.path.relpath(file_path, start=p)
+                    zipf.write(file_path, file_path)
+    return
     # with zipfile.ZipFile(f"{world}.zip", 'w', zipfile.ZIP_DEFLATED) as zipf:
     #     for root, _, files in os.walk(path):
     #         for file in files:
